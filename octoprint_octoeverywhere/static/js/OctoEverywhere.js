90a10c2d0f92e6d45ca911266d2f68df7f89f4da
Quinn Damerell <qdamere@microsoft.com>
2020-11-09 21:54:02 -0800

    Attempting another fix at the wizard.

--- octoprint_octoeverywhere/static/js/OctoEverywhere.js
+++ octoprint_octoeverywhere/static/js/OctoEverywhere.js
@@ -4,34 +4,34 @@
  * Author: Quinn Damerell
  * License: AGPLv3
  */
-$(function() {
-    function OctoeverywhereViewModel(parameters) {
-        var self = this;
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+// $(function() {
+//     function OctoeverywhereViewModel(parameters) {
+//         var self = this;
+//         // assign the injected parameters, e.g.:
+//         // self.loginStateViewModel = parameters[0];
+//         // self.settingsViewModel = parameters[1];
 
-        // self.onWizardDetails = function (response) {
-        //     if (!response.hasOwnProperty("octoeverywhere"))
-        //     {
-        //         return;
-        //     } 
-        //     if (response.octoeverywhere.details.hasOwnProperty("AddPrinterUrl"))
-        //     {
-        //         // This doesn't actually work yet.
-        //     }
-        // };        
-        // self.onWizardFinish = function() {
-        // }
-    }
+//         // self.onWizardDetails = function (response) {
+//         //     if (!response.hasOwnProperty("octoeverywhere"))
+//         //     {
+//         //         return;
+//         //     } 
+//         //     if (response.octoeverywhere.details.hasOwnProperty("AddPrinterUrl"))
+//         //     {
+//         //         // This doesn't actually work yet.
+//         //     }
+//         // };        
+//         // self.onWizardFinish = function() {
+//         // }
+//     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
-    // OCTOPRINT_VIEWMODELS.push({
-    //     construct: OctoeverywhereViewModel,
-    //     dependencies: ["wizardViewModel"],
-    //     elements: ["#wizard_plugin_octoeverywhere"]
-    // });
-});
+//     /* view model class, parameters for constructor, container to bind to
+//      * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+//      * and a full list of the available options.
+//      */
+//     // OCTOPRINT_VIEWMODELS.push({
+//     //     construct: OctoeverywhereViewModel,
+//     //     dependencies: ["wizardViewModel"],
+//     //     elements: ["#wizard_plugin_octoeverywhere"]
+//     // });
+// });
