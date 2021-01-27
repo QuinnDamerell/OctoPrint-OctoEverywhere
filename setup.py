5bdc1342a246d6f7763edaf22ce76e8ff4b9194f
Quinn Damerell <quinnd@outlook.com>
2021-01-27 12:37:37 -0800

    Oops. I bumped the version number backwards. How did I even do that?

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_octoeverywhere"
 plugin_name = "octoeverywhere"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.5.1"
+plugin_version = "0.5.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
