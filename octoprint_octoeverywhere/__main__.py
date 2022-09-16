9de66db98ef38b4972eca533c5d46f15dca899ab
Quinn Damerell <quinnd@outlook.com>
2022-09-16 14:33:26 -0700

    Updating the dev connection plugin version.

--- octoprint_octoeverywhere/__main__.py
+++ octoprint_octoeverywhere/__main__.py
@@ -130,5 +130,5 @@ if __name__ == '__main__':
 
     uiPopInvoker = UiPopupInvokerStub(logger)
     statusHandler = StatusChangeHandlerStub(logger, printerId)
-    oe = OctoEverywhere(OctoEverywhereWsUri, printerId, privateKey, logger, uiPopInvoker, statusHandler, "1.6.5")
+    oe = OctoEverywhere(OctoEverywhereWsUri, printerId, privateKey, logger, uiPopInvoker, statusHandler, "1.9.16")
     oe.RunBlocking()
