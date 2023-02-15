2abc8ba1c00d71a1852d3af3312c78bc7305a031
Quinn Damerell <quinnd@outlook.com>
2023-02-15 10:56:07 -0800

    Fixing lint erorrs

---
+++ runpylint.sh
@@ -0,0 +1,6 @@
+echo "Testing OctoEverywhere Module..."
+pylint ./octoeverywhere/
+echo "Testing OctoPrint Module..."
+pylint ./octoprint_octoeverywhere/
+echo "Testing Moonraker Module..."
+pylint ./moonraker_octoeverywhere/
