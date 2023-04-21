421abf7532e6280fbf2d82afda9dbe6677229ffc
Quinn Damerell <quinnd@outlook.com>
2023-04-21 10:44:20 -0700

    Fixing a webcam compat issue with OctoPrint 1.9

--- octoprint_octoeverywhere/octoprintwebcamhelper.py
+++ octoprint_octoeverywhere/octoprintwebcamhelper.py
@@ -27,6 +27,19 @@ class OctoPrintWebcamHelper():
         flipV = self.OctoPrintSettingsObject.global_get(["webcam", "flipV"])
         rotate90 = self.OctoPrintSettingsObject.global_get(["webcam", "rotate90"])
 
+        # TODO - In OctoPrint 1.9 the webcam was moved to a plugin model, such that plugins can implement any kind of webcams they want.
+        # There's a backwards compat layer that should keep things like the old calls above working, but it only seems to work for `snapshot` and `stream`.
+        # So we will try to get the values from the `classicwebcam` plugin, which is the default OctoPrint plugin for webcams now.
+        #
+        # In the future, we should switch to the new `octoprint.webcams.get_webcams` static API in OctoPrint (just import it and call)
+        # To get a list of webcams and handle their properties. But that will only exist in 1.9+, so for now we don't bother.
+        if flipH is None:
+            flipH = self.OctoPrintSettingsObject.global_get(["plugins", "classicwebcam", "flipH"])
+        if flipV is None:
+            flipV = self.OctoPrintSettingsObject.global_get(["plugins", "classicwebcam", "flipV"])
+        if rotate90 is None:
+            rotate90 = self.OctoPrintSettingsObject.global_get(["plugins", "classicwebcam", "rotate90"])
+
         # These values must exist, so if they don't default them.
         # TODO - We could better guess at either of these URLs if one doesn't exist and the other does.
         # We default to the relative paths, since our http request class can better manipulate these if needed.
