ade101126b025265bc3655cd77e6290934a26311
Quinn Damerell <quinnd@outlook.com>
2023-10-13 12:17:12 -0700

    Fixng update script for the root user

--- update.sh
+++ update.sh
@@ -23,21 +23,37 @@ echo -e "${c_yellow}Starting an OctoEverywhere plugin or companion update!${c_de
 echo ""
 echo ""
 
+# Since our cron update script runs as root and git commands have to be ran by the owner,
+# when we run the git commands, we need to make sure we are the right user.
+runAsRepoOwner()
+{
+    updateScriptOwner=$(stat -c %U update.sh)
+    if [[ $(whoami) == *${updateScriptOwner}* ]]; then
+        eval $1
+    else
+        sudo su - ${updateScriptOwner} -c "$1"
+    fi
+}
+
 # Pull the repo to get the top of master.
 echo "Updating repo and fetching the latest released tag..."
-git checkout master > /dev/null 2> /dev/null
-git fetch
+runAsRepoOwner "git checkout master > /dev/null 2> /dev/null"
+runAsRepoOwner "git fetch"
 
 # Find the latest tag, pull to that. We do this so we only get "released" master changes.
-latestTag=$(git describe --abbrev=0 --tags)
-currentGitStatus=$(git describe)
+latestTag=$(runAsRepoOwner "git describe --abbrev=0 --tags")
+currentGitStatus=$(runAsRepoOwner "git describe")
 echo "Latest git tag found ${latestTag}, current status ${currentGitStatus}"
 
 # Reset any local changes and pull to the tag.
-git reset --hard --quiet
-git checkout ${latestTag} --quiet
+runAsRepoOwner "git reset --hard --quiet"
+runAsRepoOwner "git checkout ${latestTag} --quiet"
 
 # Our installer script has all of the logic to update system deps, py deps, and the py environment.
 # So we use it with a special flag to do updating.
 echo "Running the update..."
 ./install.sh -update
+
+if $needsToExitUserSu ; then
+    exit
+fi
