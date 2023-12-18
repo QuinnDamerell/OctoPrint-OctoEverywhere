d7a963632f11f08004eb16097ddb87122360e19d
Quinn Damerell <quinnd@outlook.com>
2023-12-18 11:11:15 -0800

    Updating our python packages to stay current and prevent security issues!

--- setup.py
+++ setup.py
@@ -68,7 +68,17 @@ plugin_license = "AGPLv3"
 #   urllib3 - There is a bug with parsing headers in versions older than 1.26.? (https://github.com/diyan/pywinrm/issues/269). At least 1.26.6 fixes it, ubt we decide to just stick with a newer version.
 #
 # Note! These also need to stay in sync with requirements.txt, for the most part they should be the exact same!
-plugin_requires = ["websocket_client>=1.6.0,<1.6.99", "requests>=2.24.0", "octoflatbuffers==2.0.5", "pillow", "certifi>=2023.7.22", "rsa>=4.9", "dnspython>=2.3.0", "httpx==0.24.0", "urllib3>=1.26.15,<1.27.0" ]
+plugin_requires = [
+    "websocket_client>=1.7.0,<1.7.99",
+    "requests>=2.31.0",
+    "octoflatbuffers==2.0.5",
+    "pillow",
+    "certifi>=2023.11.17",
+    "rsa>=4.9",
+    "dnspython>=2.4.0",
+    "httpx>=0.25.0,<0.26.0",
+    "urllib3>=2.1.0"
+    ]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
